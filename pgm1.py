d972d1b5d43fbb14bf93a37818114baf993206e9
jyothypravi <91977226+jyothypravi@users.noreply.github.com>
2022-01-18 13:09:23 +0530

    Add files via upload

---
+++ pgm1.py
@@ -0,0 +1,8 @@
+no_int = 123
+no_flo = 1.23
+no_new = no_int + no_flo
+print("datatype of no_int:",type(no_int))
+print("datatype of no_flo:",type(no_flo))
+
+print("Value of no_new:",no_new)
+print("datatype of no_new:",type(no_new))
